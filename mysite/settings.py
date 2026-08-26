96617aee5891fe12d7b7b04c959be82eca0bb880
YuriGConstantino <yuri.gconstantino@gmail.com>
2026-08-26 19:34:08 -0300

    feat:refactor database configuration to use environment variable for production

--- mysite/settings.py
+++ mysite/settings.py
@@ -100,12 +100,24 @@ CORS_ALLOWED_ORIGINS = [
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL", f"sqlite:///{str(BASE_DIR / 'db.sqlite3')}"),
-        conn_max_age=600,
-    )
-}
+DATABASES = {}
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
