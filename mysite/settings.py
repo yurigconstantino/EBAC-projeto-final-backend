f98e9a889f92cfcf50925f7c57ae3a66a7f1387e
Yuri Gabriel Constantino <106029861+yurigconstantino@users.noreply.github.com>
2026-09-01 19:54:40 -0300

    fix: ALLOWED_HOSTS for production environment

--- mysite/settings.py
+++ mysite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-#gx(u14ab(+ikzxcdv!5mkj4d1aa!1+sjddkf1vf2!suj*0q#m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ebac-projeto-final-backend-tau.vercel.app",]
+ALLOWED_HOSTS = ["ebac-projeto-final-backend.onrender.com",]
 
 
 # Application definition
